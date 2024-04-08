4de4545cbd6dbf88ed037c74e76f6d7dd12a1f15
syed1006 <syedsalman1006@gmail.com>
2024-04-08 14:00:59 +0530

    added sidebar and info bar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import Sidebar from '@/components/sidebar';
+import React from 'react';
 
-type Props = { children: React.ReactNode }
+type Props = {children: React.ReactNode};
 
 const Layout = (props: Props) => {
-  return (
-    <div className="flex overflow-hidden h-screen">
-      <div className="w-full">
-        {props.children}
-      </div>
-    </div>
-  )
-}
+    return (
+        <div className="flex overflow-hidden h-screen">
+            <Sidebar />
+            <div className="w-full">{props.children}</div>
+        </div>
+    );
+};
 
-export default Layout
+export default Layout;
