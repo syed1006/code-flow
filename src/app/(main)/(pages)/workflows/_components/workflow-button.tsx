dc6e6ba8f2fc88cd5ef1fba92f3abab32de7e1e3
syed1006 <syedsalman1006@gmail.com>
2024-04-10 11:47:16 +0530

    Added custom modal

--- src/app/(main)/(pages)/workflows/_components/workflow-button.tsx
+++ src/app/(main)/(pages)/workflows/_components/workflow-button.tsx
@@ -1,27 +1,27 @@
 'use client';
-// import Workflowform from '@/components/forms/workflow-form'
-// import CustomModal from '@/components/global/custom-modal'
+import Workflowform from '@/components/forms/workflow-form';
+import CustomModal from '@/components/globals/custom-modal';
 import {Button} from '@/components/ui/button';
 // import { useBilling } from '@/providers/billing-provider'
-// import { useModal } from '@/providers/modal-provider'
+import {useModal} from '@/providers/modal-provider';
 import {Plus} from 'lucide-react';
 import React from 'react';
 
 type Props = {};
 
 const WorkflowButton = (props: Props) => {
-    //   const { setOpen, setClose } = useModal()
+    const {setOpen, setClose} = useModal();
     //   const { credits } = useBilling()
 
     const handleClick = () => {
-        // setOpen(
-        //   <CustomModal
-        //     title="Create a Workflow Automation"
-        //     subheading="Workflows are a powerfull that help you automate tasks."
-        //   >
-        //     <Workflowform />
-        //   </CustomModal>
-        // )
+        setOpen(
+            <CustomModal
+                title="Create a Workflow Automation"
+                subheading="Workflows are a powerfull that help you automate tasks."
+            >
+                <Workflowform />
+            </CustomModal>
+        );
     };
 
     return (
--- src/app/(main)/(pages)/workflows/page.tsx
+++ src/app/(main)/(pages)/workflows/page.tsx
@@ -8,8 +8,8 @@ const page = (props: Props) => {
         <div className="relative flex flex-col gap-4">
             <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
                 Workflows
+                <WorkflowButton />
             </h1>
-            <WorkflowButton />
         </div>
     );
 };
